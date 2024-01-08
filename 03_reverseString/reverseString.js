981893a607f9f04ee8b63d73ac6c50588f1fcf4f
jacegonzales25 <jacegonzales25@gmail.com>
2024-01-08 14:06:30 +0800

    feat: finish some javaScript exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let splitString = string.split("");
+  let newString = "";
+  for (let i = string.length - 1; i > -1; i--) {
+    newString += splitString[i];
+  }
+  return newString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elementsToRemove) {
+    // let removeIndex = arr.indexOf(params);
 
+    elementsToRemove.forEach(element => {
+        const index = arr.indexOf(element);
+        if (index !== -1) {
+          arr.splice(index, 1);
+        }
+      });
+    
+      // Alternatively, you can use the filter method:
+      // arr = arr.filter(item => !elementsToRemove.includes(item));
+    
+      return arr;
 };
 
 // Do not edit below this line
